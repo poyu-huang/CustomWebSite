05cc92dc8eb8770081caa45ac26dbece6445c571
poyuhuang <poyuhuang@microsoft.com>
2022-08-22 15:51:45 +0800

    gogo

--- CashAppTests/Program.cs
+++ CashAppTests/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-aaaa
+﻿using System;
+
+namespace CashAppTests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            aaaaaa
+        }
+    }
+}
