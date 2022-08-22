5ef08db3bcf1cb30bb5da792d4ee570905bb0d4e
poyu-huang <110226389+poyu-huang@users.noreply.github.com>
2022-08-22 16:22:54 +0800

    Update Program.cs

--- BookmarkApp/Program.cs
+++ BookmarkApp/Program.cs
@@ -6,7 +6,7 @@ namespace BookmarkApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!2");
+            Console.WriteLine("Hello World!23333");
         }
     }
 }
